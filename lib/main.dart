9a5557ca9fae52be3667e861bffda24f029147ad
rabiulsarker <rabiulsarker@polygontech.xyz>
2024-02-19 01:13:33 +0600

    flutter driver dependencies added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:demo_blog/feature/auth/login/presentation/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_driver/driver_extension.dart';
 
 void main() {
+  enableFlutterDriverExtension();
   runApp(const MyApp());
 }
 
