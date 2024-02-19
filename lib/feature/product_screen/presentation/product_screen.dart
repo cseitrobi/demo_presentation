6a7a972868bc03de6b5a278233df3496581b67b3
dipu0 <c.dipu0@gmail.com>
2024-02-19 16:44:37 +0600

    [wip]aaded home screen

--- lib/feature/product_screen/presentation/product_screen.dart
+++ lib/feature/product_screen/presentation/product_screen.dart
@@ -1,29 +1,12 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:demo_blog/feature/cart_screen/data/manage_cart.dart';
+import 'package:demo_blog/feature/product_screen/data/ProductGenerator.dart';
 import 'package:demo_blog/feature/product_screen/data/product.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
 
 class ProductScreen extends StatelessWidget {
-  final List<Product> products = List.generate(
-    10,
-    (index) => Product(
-      id: 'prod_${index + 1}',
-      title: 'Product ${index + 1}',
-      brand: 'Brand ${(Random().nextDouble() * 100).floor()}',
-      price: (Random().nextDouble() * 100).toDouble(),
-      imageUrl:
-          'https://prd.place/400/600?id=${index}', // Placeholder image URL
-      description: 'This is a detailed description of Product ${index + 1}.',
-      category: 'Category ${(Random().nextInt(5) + 1)}',
-      stockQuantity: Random().nextInt(50) + 1,
-      rating: Random().nextDouble() * 5,
-      creationDate: DateTime.now(),
-      discountedPrice: Random().nextBool() ? Random().nextDouble() * 50 : null,
-      isFeatured: Random().nextBool(),
-      tags: ['Tag1', 'Tag2', 'Tag3'],
-    ),
-  );
+  final List<Product> products = ProductGenerator.generateProducts();
 
   @override
   Widget build(BuildContext context) {
